2d8a1250a7ba9772544834f57703138e33532cdb
jcarlosdf <josedelgado1987@gmail.com>
2022-11-15 20:56:42 -0500

    second commit

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-const express = require('express')
+const express = require("express");
 // const file = requiere('./package.json')
 
-const app = express()
+const app = express();
 
-app.get('/profile', (req, res)=>{
-    res.json({
-        name: "Jose Carlos Delgado",
-        age: 35,
-        country: "Ecuador"
-    })
-})
+app.get("/profile", (req, res) => {
+  res.json({
+    name: "Jose Carlos Delgado",
+    age: 35,
+    country: "Ecuador",
+  });
+});
 
-app.post('/profile', (req, res)=>{
-    res.json(["Google", 
-                "Facebook", 
-                "Globant"
-    ])
-})
+app.post("/ejemplo", (req, res) => {
+  res.json(["Google", "Facebook", "Globant"]);
+});
 
-app.patch('/profile', (req, res)=>{
-    res.json(["Code", 
-                "3D Design", 
-                "Structures"
-    ])
-})
-const port = 3000
-app.listen(port, ()=>{
-    console.log(`Server running at port ${port}`)
-})
+app.patch("/ejemplo", (req, res) => {
+  res.json(["Code", "3D Design", "Structures"]);
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
